ce84e0c1158c8e07cf6874a4f9db30ab6301d2af
Manan Soni <manansoni1399@gmail.com>
2020-10-28 02:32:40 +0530

    complete basic single threaded server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,8 +16,21 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0;1024];
     stream.read(&mut buffer).unwrap();
     //println!("received request: {}", String::from_utf8_lossy(&buffer[..]));
-    let content = fs::read_to_string("index.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", content);
+
+    let (status_line, filename) =
+    if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else if buffer.starts_with(b"GET /sleep HTTP/1.1\r\n") {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "error.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
